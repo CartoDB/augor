9cab469ee3b8111c5653da5542e9c578bb286ebc
John Krauss <irving.krauss@gmail.com>
2015-11-04 15:47:38 -0500

    example

--- run.sh
+++ run.sh
@@ -4,3 +4,6 @@ INPUT=$1
 WC=$(wc -l $1)
 FILESIZE=$(echo $WC | cut -d ' ' -f 1)
 time cat $1 | python augment.py $2 $3 $4 | pv -a -p -e -l -s $FILESIZE
+
+# example (if you have created a `temp` table with appropriate columns):
+# ./run.sh ../data/public.csv 5 6 censustracts | psql -d census -c 'copy temp from stdin with csv'
